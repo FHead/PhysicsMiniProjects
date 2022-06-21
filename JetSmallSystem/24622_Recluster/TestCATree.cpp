60a5eceb3b76687897fa5e639dd8804ad44620d9
FHead <chen.yi.first@gmail.com>
2022-06-21 17:25:17 +0200

    documentation

--- JetSmallSystem/24622_Recluster/TestCATree.cpp
+++ JetSmallSystem/24622_Recluster/TestCATree.cpp
@@ -6,24 +6,30 @@ using namespace std;
 
 int main()
 {
+   // Declare the node object vectors
    vector<Node *> NodesEScheme, NodesWTAScheme;
 
+   // Loop over jet constituents.  Here we use a toy with 25 particles
    for(int i = 0; i < 25; i++)
    {
+      // Set particle kinematics
       FourVector P;
-
       P.SetPtEtaPhiMass(10 + i * 0.0001, (i / 5) * 0.01, (i % 5) * 0.01, 0);
 
+      // Add into the node object vector
       NodesEScheme.push_back(new Node(P));
       NodesWTAScheme.push_back(new Node(P));
    }
 
+   // Do the reclustering!
    BuildCATree(NodesEScheme, -1, EScheme);
    BuildCATree(NodesWTAScheme, -1, WTAScheme);
 
+   // Some output
    cout << "EScheme: " << NodesEScheme[0]->P.GetEta() << " " << NodesEScheme[0]->P.GetPhi() << endl;
    cout << "WTAScheme: " << NodesWTAScheme[0]->P.GetEta() << " " << NodesWTAScheme[0]->P.GetPhi() << endl;
 
+   // Clean up is important.  We only need to delete the root node and everything will be cleaned
    delete NodesEScheme[0], NodesWTAScheme[0];
 
    return 0;
--- JetSmallSystem/24622_Recluster/readme.md
+++ JetSmallSystem/24622_Recluster/readme.md
@@ -4,8 +4,8 @@
 You need to include CATree.h and make a vector of `Node *` object.
 
 Then call `BuildCATreE(vector<Node *> &Nodes, double p, int Scheme)` to do the reclustering.
-- p = 0 => CA, p = -1 => AntiKT
-- Scheme = EScheme or WTAScheme
+- `p = 0` => Cambridge-Aachen, `p = -1` => anti-KT clustering
+- `Scheme` = `EScheme` or `WTAScheme`
 
 After it's done, the head node (`Nodes[0]`) is the full reclustered jet.  We can then access the axis by
 
