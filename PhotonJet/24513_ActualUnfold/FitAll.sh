9c65283618462d2528c539040f11a1c41eb8e3f3
FHead <chen.yi.first@gmail.com>
2023-03-22 13:15:10 +0100

    photon jet

--- PhotonJet/24513_ActualUnfold/FitAll.sh
+++ PhotonJet/24513_ActualUnfold/FitAll.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in `ls MollyInput20220518/`
+for i in `ls MollyInput20220523/`
 do
    echo $i
-	./Execute --Input MollyInput20220518/$i \
-      --Output MollyOutput20220518/$i \
+	./Execute --Input MollyInput20220523/$i \
+      --Output MollyOutput20220523/$i \
 		--Prior MC \
       --DoBayes true --DoRepeatedBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
 done
